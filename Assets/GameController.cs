1a0d62205841e2f04af8c2bc68c32944f6d593dd
DESKTOP-KE05CFV\User <filip_dobrinic@bitbucket.org>
2019-09-21 22:57:44 +0200

    simulation setup scene

---
+++ Assets/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    #endregion
+}
