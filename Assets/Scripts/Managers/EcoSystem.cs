6939b0b588d1c83c607a61cce995aa4f91805fa5
DESKTOP-KE05CFV\User <filip_dobrinic@bitbucket.org>
2019-09-29 21:46:21 +0200

    EcoSystem: started to create the root of the system

---
+++ Assets/Scripts/Managers/EcoSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace EcoSystem
+{
+	public class EcoSystem
+	{
+		#region Members
+
+		public List<Animal> LivingAnimals { get; set; }
+		public List<Animal> DeadAnimals { get; set; }
+
+		#endregion
+		//----------------------------------------------------------------------------------------------------------------------------
+		#region Public
+
+		public EcoSystem(List<Animal> initialAnimals)
+		{
+			LivingAnimals = initialAnimals;
+		}
+
+		public void AddAnimal(Animal animal)
+		{
+			LivingAnimals.Add(animal);
+		}
+
+		public void RemoveAnimal(Animal animal)
+		{
+			LivingAnimals.Remove(animal);
+			DeadAnimals.Add(animal);
+		}
+
+		#endregion
+	}
+}
