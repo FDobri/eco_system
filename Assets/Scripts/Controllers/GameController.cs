bd01bcb5a6c33b26900ce763bb2482ebab719e11
DESKTOP-KE05CFV\User <filip_dobrinic@bitbucket.org>
2019-09-29 18:13:23 +0200

    simulation setup scene

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	#endregion
+	// nunununununununununununununununununununununununununununununun
+	#region Private
+
+
+	#endregion
+	// nunununununununununununununununununununununununununununununun
+	#region Public
+
+	#endregion
+	// nunununununununununununununununununununununununununununununun
+}
---
+++ Assets/Scripts/Views/SimulationSetupPanelView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimulationSetupPanelView : MonoBehaviour
+{
+	#region Members
+
+	private const string SIMULATION_SCENE_NAME = "Scene_01";
+
+	#endregion
+	// nunununununununununununununununununununununununununununununun
+	#region Public
+
+	public void LaunchScene()
+	{
+		SceneManager.LoadScene(SIMULATION_SCENE_NAME);
+	}
+
+	#endregion
+}
